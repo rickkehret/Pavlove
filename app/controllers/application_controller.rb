d05c6c82e801cea52d044d1d807d2ba14098d849
Rick Kehret <rick.kehret@gmail.com>
2011-12-22 06:54:11 -0800

    finished basic layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def home
-    @title = "Home"
-  end
-
-  def contact
-    @title = "Contact"
-  end
-
-  def about
-    @title = "About"
-  end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   # GET /users/new
   # GET /users/new.json
   def new
+    @title = "Sign Up"
     @user = User.new
 
     respond_to do |format|
